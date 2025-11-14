01756c558bab74d6175b0cc064313d06cdfda4f8
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-11-14 13:19:42 +0200

    Add book table to database

--- src/schema.sql
+++ src/schema.sql
@@ -2,4 +2,13 @@ CREATE TABLE todos (
   id SERIAL PRIMARY KEY, 
   content TEXT NOT NULL,
   done BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE book (
+  id SERIAL PRIMARY KEY,
+  title TEXT,
+  author TEXT,
+  year INTEGER,
+  publisher TEXT,
+  address TEXT
 )
