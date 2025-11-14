977d6ce4e0054527bf3021072193fa1b113dcfd0
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-11-14 14:35:01 +0200

    Fix pylint suggestions

--- src/repositories/book_repository.py
+++ src/repositories/book_repository.py
@@ -4,6 +4,7 @@ from sqlalchemy import text
 from entities.todo import Book
 
 def get_books():
-    result = db.session.execute(text("SELECT id, title, author, year, publisher, address FROM book"))
+    query = text("SELECT id, title, author, year, publisher, address FROM book")
+    result = db.session.execute(query)
     books = result.fetchall()
     return [Book(book[0], book[1], book[2], book[3], book[4], book[5]) for book in books]
