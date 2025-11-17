65186e4a8a2f21aef12fcfaf0725c2aa83999f0c
Aaron <119842456+kahkaar@users.noreply.github.com>
2025-11-17 14:23:21 +0200

    Fix Book import in book_repository

--- src/repositories/book_repository.py
+++ src/repositories/book_repository.py
@@ -1,10 +1,12 @@
-from config import db
 from sqlalchemy import text
 
-from entities.todo import Book
+from config import db
+from entities.book import Book
+
 
 def get_books():
-    query = text("SELECT id, title, author, year, publisher, address FROM book")
+    query = text(
+        "SELECT id, title, author, year, publisher, address FROM book")
     result = db.session.execute(query)
     books = result.fetchall()
     return [Book(book[0], book[1], book[2], book[3], book[4], book[5]) for book in books]
